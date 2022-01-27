6df8a42350fd6aafc8cef177b1b632609380ff2d
Stephan Reindl <stephan.reindl@gmx.at>
2022-01-27 10:41:44 +0100

    added basic3

--- Basic2/script.js
+++ Basic2/script.js
@@ -1,10 +1,9 @@
-function ageCalculator(birthYear) {
-    var year1 = new Date().getFullYear();
+function ageCalculator(birthYear, currentYear) {
+    var year1 = currentYear;
     var year2 = birthYear;
     var result = year1 - year2;
     var arr = [result, result - 1]
     return `You are either ${arr[0]} or ${arr[1]}`;
-};
-// var year1 = new Date().getFullYear();
-// console.log(year1);
-console.log(ageCalculator(1999));
+}
+
+console.log(ageCalculator(1988, 2022));
