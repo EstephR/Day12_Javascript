5e21d866c89237931536cc018a4058e8f4092ad9
Stephan Reindl <stephan.reindl@gmx.at>
2022-01-31 11:53:28 +0100

    added Intermediate 3

---
+++ Live Coding Serri/script.js
@@ -0,0 +1,46 @@
+// var number = Math.floor(Math.random() * 41) - 10;
+
+// console.log(number);
+
+// if (number <= 3) {
+//     console.log("Weather is frezing")
+// } else if (number <= 10) {
+//     console.log("Weather is cold");
+// } else if (number <= 22) {
+//     console.log("Weather is intermediate");
+// } else {
+//     console.log("Weather is Hot");
+// }
+// var name = "Serri";
+
+// switch (number) {
+//     case 0:
+//         console.log("Weather is frezing");
+//         break;
+
+
+//     case 1:
+//         console.log("Weather is cold");
+//         break;
+
+//     case (number <= 22):
+//         console.log("Weather is intermediate");
+//         break;
+
+//     default:
+//         console.log("Weather is Hot");
+//         break;
+// }
+// function showMeNumber(i) {
+//     var num = document.getElementById("input").value;
+//     console.log(num, i);
+// }
+
+// // document.getElementById("btn").onclick = function() {
+// //     showMeNumber(7);
+// //     console.log("Hello from console")
+// // };
+
+// document.getElementById("btn").addEventListener("click", function() {
+//     showMeNumber(9)
+// }, false);
